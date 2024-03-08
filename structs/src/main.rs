75287496f8bc906225a286a14c1cdc6a42b1966e
Rudianto Sihombing <rudi97277@gmail.com>
2024-03-08 17:48:57 +0700

    struct

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,41 +1,14 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
-    let mut user1 = build_user(String::from("rudi@gmail.com"), String::from("rudi97278"));
-
-    println!(" name {}", user1.username);
-    println!(" name {}", user1.username);
-
-    let mut user2 = User {
-        email: String::from("rudi9@gmail.com"),
-        ..user1
+    let rect1 = Rectangle {
+        width: 30,
+        height: 20,
     };
 
-    let user1 = User2 {
-        active: true,
-        username: "someusername123",
-        email: "someone@example.com",
-        sign_in_count: 1,
-    };
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        active: true,
-        username,
-        email,
-        sign_in_count: 1,
-    }
-}
-
-struct User2 {
-    active: bool,
-    username: &str,
-    email: &str,
-    sign_in_count: u64,
-}
-
-struct User {
-    active: bool,
-    username: String,
-    email: String,
-    sign_in_count: u64,
+    dbg!(&rect1);
 }
