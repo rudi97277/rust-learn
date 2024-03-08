33a33fad94f15b3a14d9db87778741631aa4f7f0
Rudianto Sihombing <rudi97277@gmail.com>
2024-03-08 18:16:13 +0700

    rect

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,7 +1,23 @@
-#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
@@ -10,5 +26,15 @@ fn main() {
         height: 20,
     };
 
-    dbg!(&rect1);
+    let rect2 = Rectangle {
+        width: 31,
+        height: 22,
+    };
+
+    let rect3 = Rectangle::square(2);
+
+    println!("value {}", rect3.area());
+
+    println!("value {}", rect1.area());
+    println!("if first can hold the second {}", rect2.can_hold(&rect1));
 }
