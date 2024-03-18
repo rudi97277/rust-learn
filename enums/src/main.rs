c0b8303a77697ea45a5580c774493154d2824f71
Rudianto Sihombing <rudi97277@gmail.com>
2024-03-18 19:07:43 +0700

    asdsa

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -9,13 +9,30 @@ struct IpAddr {
     address: String,
 }
 
+#[derive(Debug)]
+enum Message {
+    Quit(i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
+
 fn main() {
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
     let _absent: Option<i32> = None;
 
     let home = IpAddr {
         kind: IpAddrKind::V4(192, 168, 0, 1),
         address: String::from("127.0.0.1"),
     };
+
+    let new = Message::Quit(32, 50);
+    new.call();
 
     route(&home);
 }
